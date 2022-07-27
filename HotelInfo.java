8a75e51dce52dbc5a97932f7f11020dc65593836
Ashu Pant <108819915+ashupant-cs22@users.noreply.github.com>
2022-07-27 18:31:51 +0530

    Weekday and Weekend cases added

---
+++ HotelInfo.java
@@ -0,0 +1,61 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Optional;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class HotelInfo {
+
+    ArrayList<Hotel> myHotelList = new ArrayList();
+
+    public void addHotel() {
+
+        Hotel LakeWood = new Hotel("LakeWood", 110, 80, 90, 80, 3);
+        Hotel BridgeWood = new Hotel("BridgeWood", 150, 110, 50, 50, 4);
+        Hotel RidgeWood = new Hotel("RidgeWood", 220, 100, 150, 40, 5);
+
+        myHotelList.add(LakeWood);
+        myHotelList.add(BridgeWood);
+        myHotelList.add(RidgeWood);
+
+        System.out.println(myHotelList);
+    }
+
+    public void CheapestHotelWeekDay(String startDateRange, String endDateRange) {
+
+        LocalDate startDate = LocalDate.parse(startDateRange, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+        LocalDate endDate = LocalDate.parse(endDateRange, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+
+        int numberOfDays = endDate.getDayOfMonth() - startDate.getDayOfMonth() + 1;
+
+        Optional<Hotel> cheapestHotel = this.myHotelList.stream().sorted(Comparator.comparing(Hotel::getWeekdayRegularRate)).findFirst();
+
+        Hotel hotel = new Hotel();
+        hotel.setHotelName(cheapestHotel.get().getHotelName());
+        hotel.setTotal(cheapestHotel.get().getWeekdayRegularRate() * numberOfDays);
+        System.out.println("HotelName :" + hotel.getHotelName());
+        System.out.println("NumberOfDaysStayed  WeekdayRegularRate :" + hotel.getTotal() + "$");
+
+    }
+
+    public void CheapestHotelWeekend(String startDateRange, String endDateRange) {
+
+        LocalDate startDate = LocalDate.parse(startDateRange, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+        LocalDate endDate = LocalDate.parse(endDateRange, DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+
+        int numberOfDays = endDate.getDayOfMonth() - startDate.getDayOfMonth() + 1;
+
+        Optional<Hotel> cheapestHotel = this.myHotelList.stream().sorted(Comparator.comparing(Hotel::getWeekendRegularRate)).findFirst();
+
+        Hotel hotel = new Hotel();
+        hotel.setHotelName(cheapestHotel.get().getHotelName());
+        hotel.setTotal(cheapestHotel.get().getWeekendRegularRate() * numberOfDays);
+
+        System.out.println("HotelName :" + hotel.getHotelName());
+        System.out.println("Number Of Days Stayed Weekend Regular Rate :" + hotel.getTotal() + "$");
+
+    }
+
+}
