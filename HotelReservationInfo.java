697b4f2c8793de9a3c596f48520477f3bde805c3
Ashu Pant <108819915+ashupant-cs22@users.noreply.github.com>
2022-07-27 17:25:20 +0530

    Added Hotel

---
+++ HotelReservationInfo.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class HotelReservationInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation Management");
+        HotelDetails hotelDetails = new HotelDetails();
+        hotelDetails.addHotel();
+    }
+}
